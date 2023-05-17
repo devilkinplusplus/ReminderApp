d45ee2e6a381510925d08b8b7f8ae67f102ec650
Ilkin Rufullayev <rufullayevilkin66@gmail.com>
2023-05-17 17:14:39 +0400

    testing

--- ReminderApp/Consts/TelegramApiInformations.cs
+++ ReminderApp/Consts/TelegramApiInformations.cs
@@ -2,7 +2,7 @@
 {
     public static class TelegramApiInformations
     {
-        public static string ApiKey = "6071762209:AAHy3hMJfAyNihBC125KBL_MODaUuZ7-w2I";
-        public static string ChatId = "1591793172";
+        public static string ApiKey = "[API_KEY]";
+        public static string ChatId = "[Chat_Id]";
     }
 }
